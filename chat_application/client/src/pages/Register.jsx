4184dbfa032afceb2f8f22d6af8da2b603d461cd
ashutosh4398 <ashutoshdhondkar@gmail.com>
2024-09-28 19:48:21 +0530

    context api for registration form

--- chat_application/client/src/pages/Register.jsx
+++ chat_application/client/src/pages/Register.jsx
@@ -1,20 +1,45 @@
-import {Alert, Button, Form, Row, Col, Stack} from "react-bootstrap";
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
+  const { registerInfo, updateRegisterInfo } = useContext(AuthContext);
   return (
     <>
       <Form>
-        <Row style={{
-            height: "100vh", 
+        <Row
+          style={{
+            height: "100vh",
             justifyContent: "center",
           }}
         >
           <Col xs={6}>
             <Stack gap={3}>
               <h2 className="text-center">Register</h2>
-              <Form.Control type="text" placeholder="Enter your name"/>
-              <Form.Control type="email" placeholder="Enter your email"/>
-              <Form.Control type="password" placeholder="Enter your password"/>
+              <Form.Control
+                type="text"
+                placeholder="Enter your name"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, name: e.target.value })
+                }
+              />
+              <Form.Control
+                type="email"
+                placeholder="Enter your email"
+                onChange={(e) =>
+                  updateRegisterInfo({ ...registerInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                onChange={(e) =>
+                  updateRegisterInfo({
+                    ...registerInfo,
+                    password: e.target.value,
+                  })
+                }
+              />
               <Button variant="primary" type="submit">
                 Signup
               </Button>
