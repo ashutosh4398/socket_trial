a0242da850a2f629c27280b1dd6ebf4ebd75052f
ashutosh4398 <ashutoshdhondkar@gmail.com>
2024-09-28 21:00:49 +0530

    handling login api integration

--- chat_application/client/src/context/AuthContext.jsx
+++ chat_application/client/src/context/AuthContext.jsx
@@ -4,47 +4,85 @@ import { postRequest } from "../utils/services";
 export const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children }) => {
+  const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [loginError, setLoginError] = useState(null);
+  const [isLoginLoading, setisLoginLoading] = useState(false);
+
   const [user, setUser] = useState(null);
   const [registerError, setRegisterError] = useState(null);
   const [isRegisterLoading, setisRegisterLoading] = useState(false);
   const [registerInfo, setRegisterInfo] = useState({
     name: "",
-    email:"",
-    password: ""
+    email: "",
+    password: "",
   });
 
-
-  useEffect(()=>{
+  useEffect(() => {
     const user = localStorage.getItem("user");
     if (!user) return;
-    setUser(JSON.parse(user))
+    setUser(JSON.parse(user));
   }, []);
-
 
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info);
   }, []);
 
-  const registerUser = useCallback(async (e) => {
-    e.preventDefault(); // preventing page refreshing on form submit;
-    setRegisterError(null);
-    setisRegisterLoading(true)
-    const response = await postRequest("/users/register", JSON.stringify(registerInfo));
-    if (response.error) {
+  const registerUser = useCallback(
+    async (e) => {
+      e.preventDefault(); // preventing page refreshing on form submit;
+      setRegisterError(null);
+      setisRegisterLoading(true);
+      const response = await postRequest(
+        "/users/register",
+        JSON.stringify(registerInfo)
+      );
+      if (response.error) {
         setRegisterError(response);
         setisRegisterLoading(false);
         return;
-    }
-    setUser(response);
-    localStorage.setItem("user", JSON.stringify(response));
-    setRegisterError(null);
-    setisRegisterLoading(false);
-  }, [registerInfo]);
+      }
+      setUser(response);
+      localStorage.setItem("user", JSON.stringify(response));
+      setRegisterError(null);
+      setisRegisterLoading(false);
+    },
+    [registerInfo]
+  );
 
   const logoutUser = useCallback(() => {
     localStorage.removeItem("user");
     setUser(null);
+    setLoginInfo({ email: "", password: "" });
   }, []);
+
+  const updateLoginInfo = useCallback((info) => {
+    setLoginInfo(info);
+  }, []);
+
+  const loginUser = useCallback(
+    async (e) => {
+      e.preventDefault();
+      setisLoginLoading(true);
+      setLoginError(null);
+      const response = await postRequest(
+        "/users/login",
+        JSON.stringify(loginInfo)
+      );
+      if (response.error) {
+        setLoginError(response);
+        setisLoginLoading(false);
+        return;
+      }
+      setUser(response);
+      localStorage.setItem("user", JSON.stringify(response));
+      setLoginError(null);
+      setisLoginLoading(false);
+    },
+    [loginInfo]
+  );
 
   return (
     <AuthContext.Provider
@@ -55,7 +93,12 @@ export const AuthContextProvider = ({ children }) => {
         registerError,
         isRegisterLoading,
         user,
-        logoutUser
+        loginInfo,
+        logoutUser,
+        isLoginLoading,
+        loginError,
+        updateLoginInfo,
+        loginUser,
       }}
     >
       {children}
--- chat_application/client/src/pages/Login.jsx
+++ chat_application/client/src/pages/Login.jsx
@@ -1,25 +1,44 @@
-import {Alert, Button, Form, Row, Col, Stack} from "react-bootstrap";
+import { useContext } from "react";
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
+  const { updateLoginInfo, loginUser, loginError, isLoginLoading, loginInfo } =
+    useContext(AuthContext);
   return (
     <>
-      <Form>
-        <Row style={{
-            height: "100vh", 
+      <Form onSubmit={loginUser}>
+        <Row
+          style={{
+            height: "100vh",
             justifyContent: "center",
           }}
         >
           <Col xs={6}>
             <Stack gap={3}>
               <h2 className="text-center">Login</h2>
-              <Form.Control type="email" placeholder="Enter your email"/>
-              <Form.Control type="password" placeholder="Enter your password"/>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, email: e.target.value })
+                }
+              />
+              <Form.Control
+                type="password"
+                placeholder="Enter your password"
+                onChange={(e) =>
+                  updateLoginInfo({ ...loginInfo, password: e.target.value })
+                }
+              />
               <Button variant="primary" type="submit">
-                Login
+                {isLoginLoading ? "Logging In..." : "Login"}
               </Button>
-              <Alert variant="danger">
-                <p>An error occured!</p>
-              </Alert>
+              {loginError && (
+                <Alert variant="danger">
+                  <p>{loginError.message}</p>
+                </Alert>
+              )}
             </Stack>
           </Col>
         </Row>
